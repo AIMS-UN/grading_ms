3de5e5b8fd839c4b77ef09f572bdf51e4c5652b4
SGman98 <bastiangarnica@gmail.com>
2022-09-09 23:42:40 -0500

    feat: 🍱 update main file to basic rocket server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::{http::Status, serde::json::Json};
+
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json("Hello from rust an rocket".to_string()))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
