2b9da8b79b9a34ad70b49619496ff02069a41fb9
SGman98 <bastiangarnica@gmail.com>
2022-09-10 19:57:02 -0500

    feat: ✨ add mongodb connection in the project main file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
+use mongodb::{bson::doc, sync::Client};
 use rocket::{http::Status, serde::json::Json};
+use std::env;
 
 #[get("/")]
 fn hello() -> Result<Json<String>, Status> {
@@ -10,5 +12,12 @@ fn hello() -> Result<Json<String>, Status> {
 
 #[launch]
 fn rocket() -> _ {
+    let uri = env::var("MONGO_URI").expect("MONGO_URI must be set");
+    let client = Client::with_uri_str(&uri).expect("Invalid URI");
+    client
+        .database("testing_db")
+        .collection("testing_collection")
+        .insert_one(doc! {"name": "test"}, None)
+        .expect("Failed to insert document.");
     rocket::build().mount("/", routes![hello])
 }
