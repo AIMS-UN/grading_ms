88f11d70c806f0faff7b6dcc642cae829dbc2976
SGman98 <bastiangarnica@gmail.com>
2022-09-10 22:21:50 -0500

    feat: ⚗️ update rocket and mongodb to async connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
-use mongodb::{bson::doc, sync::Client};
+use mongodb::{bson::doc, Client};
 use rocket::{http::Status, serde::json::Json};
 use std::env;
 
@@ -10,14 +10,21 @@ fn hello() -> Result<Json<String>, Status> {
     Ok(Json("Hello from rust an rocket".to_string()))
 }
 
-#[launch]
-fn rocket() -> _ {
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
     let uri = env::var("MONGO_URI").expect("MONGO_URI must be set");
-    let client = Client::with_uri_str(&uri).expect("Invalid URI");
+    let client = Client::with_uri_str(&uri)
+        .await
+        .expect("Failed to initialize client.");
+
     client
         .database("testing_db")
         .collection("testing_collection")
         .insert_one(doc! {"name": "test"}, None)
+        .await
         .expect("Failed to insert document.");
-    rocket::build().mount("/", routes![hello])
+
+    let _rocket = rocket::build().mount("/", routes![hello]).launch().await?;
+
+    Ok(())
 }
